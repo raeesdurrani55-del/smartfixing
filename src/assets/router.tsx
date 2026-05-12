4c235fbf8e25a1e55c185047054da4e94b9ee6f8
raeesdurrani55-del <raeesdurrani55@gmail.com>
2026-05-12 23:07:11 +0500

    Update router.tsx

--- src/assets/router.tsx
+++ src/assets/router.tsx
@@ -2,7 +2,7 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export const createRouter = () => {
+export function createRouter() {
   const queryClient = new QueryClient();
 
   const router = createTanStackRouter({
@@ -13,4 +13,10 @@ export const createRouter = () => {
   });
 
   return router;
-};
+}
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
